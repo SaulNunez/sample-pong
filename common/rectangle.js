3e56e2ffa638f1375366df2d1b2fb7240a032e39
Abel Rodríguez <abelrodriguez.e@gmail.com>
2018-10-21 13:24:21 -0600

    Se creó la clase rectángulo.

--- common/rectangle.js
+++ common/rectangle.js
@@ -1,3 +1,7 @@
+/**
+ * Clase lógica de rectángulo.
+ * Utilizada sólo para tener un manejo común de los rectangulos.
+ */
 class Rectangle {
   constructor(x = 0, y = 0, width = 0, height = 0) {
     this.x = x;
@@ -6,19 +10,38 @@ class Rectangle {
     this.height = height;
   }
 
+  /**
+   * Propiedad tipo get para obtener la coordenada superior del rectángulo.
+   */
   get top () {
     return this.y;
   }
 
+  /**
+   * Propiedad tipo get para obtener la coordenada inferior del rectángulo.
+   */
   get bottom() {
     return this.y + this.height;
   }
 
+  /**
+   * Propiedad tipo get para obtener la coordenada izquierda del rectángulo.
+   */
   get left() {
     return this.x;
   }
 
+  /**
+   * Propiedad tipo get para obtener la coordenada derecha del rectángulo.
+   */
   get right() {
     return this.x + this.width;
+  }
+
+  /**
+   * Dibuja el rectángulo.
+   */
+  draw() {
+    drawRectangle(this.x, this.y, this.width, this.height);
   }
 }
