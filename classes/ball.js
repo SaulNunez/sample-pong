b7867b6eafc77317410436bf4d5faa0ec226d631
Abel Rodríguez <abelrodriguez.e@gmail.com>
2018-10-21 12:54:33 -0600

    Se creó el esqueleto de las clases Ball y Player.

---
+++ classes/ball.js
@@ -0,0 +1,25 @@
+class Ball {
+  constructor() {
+
+  }
+
+  /**
+   * Función llamada cada fotograma.
+   * Método utilizado para manejar toda la lógica referente a los objetos tipo Ball.
+   * @param {number} deltaTime
+   * @param {number} ms
+   */
+  update(deltaTime, ms) {
+
+  }
+
+  /**
+   * Función llamada cada fotograma.
+   * Método utilizado para manejar toda la lógica de renderizado de los objetos tipo Ball
+   * @param {number} deltaTime
+   * @param {number} ms
+   */
+  draw(deltaTime, ms) {
+
+  }
+}
---
+++ classes/player.js
@@ -0,0 +1,25 @@
+class Player {
+  constructor() {
+
+  }
+
+  /**
+   * Función llamada cada fotograma.
+   * Método utilizado para manejar toda la lógica referente a los objetos tipo Player.
+   * @param {number} deltaTime
+   * @param {number} ms
+   */
+  update(deltaTime, ms) {
+
+  }
+
+  /**
+   * Función llamada cada fotograma.
+   * Método utilizado para manejar toda la lógica de renderizado de los objetos tipo Player
+   * @param {number} deltaTime
+   * @param {number} ms
+   */
+  draw(deltaTime, ms) {
+
+  }
+}
